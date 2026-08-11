52f33e2b164b15cc5213e28315f9b47ce173b183
Jagoda Połchowska <j.polchowska.252@studms.ug.edu.pl>
2026-08-11 15:31:57 +0200

    feat: add login screen UI

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,7 +1,13 @@
 import '../global.css';
 
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
